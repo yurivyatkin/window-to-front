265c8fd8b8f07d668bfd4ad36cdc2e49cac5d114
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-10-26 22:17:59 +1300

    Expose plugin's functionality

--- lib/window_to_front.dart
+++ lib/window_to_front.dart
@@ -1,13 +1,14 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class WindowToFront {
   static const MethodChannel _channel = MethodChannel('window_to_front');
-
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  // Add from here
+  static Future<void> activate() {
+    return _channel.invokeMethod('activate');
   }
+  // to here.
+
+  // Delete the getPlatformVersion getter method.
 }
